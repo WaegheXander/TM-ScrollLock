aec63c91115276f219eda7cb01ddfa8a1fe10544
Xander Waeghe <91458842+WaegheXander@users.noreply.github.com>
2022-12-27 12:13:40 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const listenToSlider = function () {
+    const slider = document.querySelector('.js-slider');
+    const array_values = [0,232,464,696,928];
+    //set transistion ptoperty to slider value
+    slider.addEventListener('input', function () {
+        document.querySelector('.js-active-item').style.setProperty('transform', 'translateX(' + array_values[slider.value] + 'px)');
+    });
+
+    //change opacity of item based on slider value
+    slider.addEventListener('input', function () {
+        document.querySelectorAll('.c-item__inner').forEach(function (item) {
+            item.style.setProperty('opacity', '0.2');
+        });
+        const activeElement = document.querySelectorAll('.c-item__inner')[slider.value];
+        activeElement.style.setProperty('opacity', '1');
+    });
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    listenToSlider();
+});
