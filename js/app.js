3b50be159a6d4a77fbf5f2305e970ce3e63ff519
Xander Waeghe <xander.waeghe@student.howest.be>
2023-01-18 22:02:31 +0100

    adding dynamic loading of route wall

--- js/app.js
+++ js/app.js
@@ -3,16 +3,17 @@ const fetchPromis = function (url) {
   return fetch(url).then((response) => response.json().catch((error) => console.log(error)));
 };
 
+let wall, wallItem, routes;
+
 document.addEventListener('DOMContentLoaded', function () {
   toggleNav();
-
   if (document.querySelector('#ropeDetail')) {
     getWallRoutes();
-    stars();
+    togggleStars();
   }
 });
 
-const stars = function () {
+const togggleStars = function () {
   const starEls = document.querySelectorAll('.star.rating');
 
   starEls.forEach((star) => {
@@ -35,8 +36,9 @@ function toggleNav() {
 }
 
 const getWallRoutes = async function () {
-  routes = await getWallRoutes('http://127.0.0.1:5500/js/dummydata.json');
-  console.log(routes);
+  // const url = 'http://127.0.0.1:5500/js/dummydata.json';
+  const url = 'https://waeghexander.github.io/TeamProject_Proto/js/dummydata.json';
+  routes = await fetchPromis(url);
   showWall();
 };
 
@@ -49,54 +51,48 @@ const showWall = function () {
   document.querySelector('.js-wall').innerHTML = html;
   showGrips();
 };
-let wall, wallItem, routes;
 
 const showGrips = async function () {
   wall = document.querySelectorAll('.c-wall');
   wallItem = document.querySelectorAll('.c-wall__grid--item');
-  routes = await getWallRoutes();
-
   for (let i = 0; i < routes.length; i++) {
-    let x = routes[i].point[0];
-    let y = routes[i].point[1];
-    let index = x - 1 + (y - 1) * 20;
-    wallItem[index].classList.add('c-wall__grid--item--grip');
-
-    switch (routes[i].handgriptype) {
-      case 'Bak':
-        wallItem[index].classList.add('c-wall__grid--item--grip--bak');
-        break;
-      case 'Sloper':
-        wallItem[index].classList.add('c-wall__grid--item--grip--sloper');
-        break;
-      case 'Crimp':
-        wallItem[index].classList.add('c-wall__grid--item--grip--crimp');
-        break;
-      case 'Pocket':
-        wallItem[index].classList.add('c-wall__grid--item--grip--pocket');
-        break;
-      case 'Jug':
-        wallItem[index].classList.add('c-wall__grid--item--grip--jug');
-        break;
-      case 'Undercling':
-        wallItem[index].classList.add('c-wall__grid--item--grip--undercling');
-        break;
-      case 'Mono':
-        wallItem[index].classList.add('c-wall__grid--item--grip--mono');
-        break;
-      case 'Bidoigt':
-        wallItem[index].classList.add('c-wall__grid--item--grip--bidoigt');
-        break;
+    for (let j = 0; j < routes[i].grips.length; j++) {
+      let index = routes[i].grips[j].point[0] - 1 + (routes[i].grips[j].point[1] - 1) * 20;
+      wallItem[index].classList.add('c-wall__grid--item--grip');
+      switch (routes[i].grips[j].handgriptype) {
+        case 'Bak':
+          wallItem[index].classList.add('c-wall__grid--item--grip--bak');
+          break;
+        case 'Sloper':
+          wallItem[index].classList.add('c-wall__grid--item--grip--sloper');
+          break;
+        case 'Crimp':
+          wallItem[index].classList.add('c-wall__grid--item--grip--crimp');
+          break;
+        case 'Pocket':
+          wallItem[index].classList.add('c-wall__grid--item--grip--pocket');
+          break;
+        case 'Jug':
+          wallItem[index].classList.add('c-wall__grid--item--grip--jug');
+          break;
+        case 'Undercling':
+          wallItem[index].classList.add('c-wall__grid--item--grip--undercling');
+          break;
+        case 'Mono':
+          wallItem[index].classList.add('c-wall__grid--item--grip--mono');
+          break;
+        case 'Bidoigt':
+          wallItem[index].classList.add('c-wall__grid--item--grip--bidoigt');
+          break;
+      }
     }
   }
-
   drawSelectedPath(1);
   listenToRouteClick();
 };
 
 const listenToRouteClick = function () {
   let options = document.querySelectorAll('.js-path-select');
-
   options.forEach((option) => {
     option.addEventListener('change', function (event) {
       let selectedPath = this.dataset.id;
@@ -108,43 +104,34 @@ const listenToRouteClick = function () {
 const drawSelectedPath = function (selectedPath) {
   const topDiffrence = wallItem[20].getBoundingClientRect().top - wallItem[0].getBoundingClientRect().top;
   const leftDiffrence = wallItem[1].getBoundingClientRect().left - wallItem[0].getBoundingClientRect().left;
-
   let cords = [];
-
   for (let i = 0; i < routes.length; i++) {
-    if (routes[i].route !== selectedPath) {
-      continue;
+    for (let j = 0; j < routes[i].grips.length; j++) {
+      if (routes[i].routeID !== selectedPath) {
+        continue;
+      }
+      let top = routes[i].grips[j].point[0] * topDiffrence - topDiffrence;
+      if (top < 0) {
+        top = 0;
+      }
+      let left = routes[i].grips[j].point[1] * leftDiffrence - leftDiffrence;
+      if (left < 0) {
+        left = 0;
+      }
+      cords.push({
+        xpoint: top,
+        ypoint: left,
+      });
     }
-
-    let top = routes[i].point[0] * topDiffrence - topDiffrence;
-
-    if (top < 0) {
-      top = 0;
-    }
-
-    let left = routes[i].point[1] * leftDiffrence - leftDiffrence;
-
-    if (left < 0) {
-      left = 0;
-    }
-
-    cords.push({
-      xpoint: top,
-      ypoint: left,
-    });
   }
-
   var points = cords;
-
   var Gen = d3
     .line()
     .x((p) => p.xpoint)
     .y((p) => p.ypoint)
     .curve(d3.curveCardinal);
-
   d3.select('#gfg').selectAll('path').remove();
   d3.select('#gfg').append('path').attr('d', Gen(points)).attr('fill', 'none').attr('stroke', '#5804f4').attr('stroke-width', '2').attr('class', 'js-path');
-
   animatePath();
 };
 
