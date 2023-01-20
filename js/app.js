12caacd5488ba91c639fe2d2c61c964feac739e9
Xander Waeghe <xander.waeghe@student.howest.be>
2023-01-20 17:12:37 +0100

    try to fixed javascrip draw non selected

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+// TODO - add a comment to explain what this file does
+// TODO - add a comment to explain how this file is structured
+// TODO - fix draw with non selected routes
+
 // #region ***  DOM references                           ***********
 let wall,
   wallItem,
   routes,
-  selectedPath = 1,
+  selectedPath = '',
   ropeId = 1;
 
 const fetchPromis = function (url) {
@@ -10,7 +14,6 @@ const fetchPromis = function (url) {
     .then((response) => {
       console.info('Data fetched');
       let res = response.json();
-      console.info(res);
       return res;
     })
     .catch((error) => {
@@ -20,6 +23,7 @@ const fetchPromis = function (url) {
 
 const toggleNav = function () {
   let toggleTrigger = document.querySelectorAll('.js-toggle-nav');
+
   for (let i = 0; i < toggleTrigger.length; i++) {
     toggleTrigger[i].addEventListener('click', function () {
       document.querySelector('body').classList.toggle('has-mobile-nav');
@@ -39,29 +43,53 @@ const togggleStars = function () {
   });
 };
 
-const drawSelectedPath = function (selectedPath) {
+const hideNonSelectedRoutes = function () {
+  wallItem = document.querySelectorAll('.c-wall__grid--item--grip');
+  for (let i = 0; i < wallItem.length; i++) {
+    let dataId = wallItem[i].getAttribute('data-id');
+    if (dataId !== selectedPath) {
+      wallItem[i].classList.add('u-opacity-half');
+      wallItem[i].classList.remove('u-opacity-full');
+      console.log('half');
+    } else {
+      wallItem[i].classList.add('u-opacity-full');
+      wallItem[i].classList.remove('u-opacity-half');
+      console.log('full');
+    }
+  }
+};
+
+const drawSelectedPath = function () {
+  console.log('draw selected path: ', selectedPath);
   const topDiffrence = wallItem[20].getBoundingClientRect().top - wallItem[0].getBoundingClientRect().top;
   const leftDiffrence = wallItem[1].getBoundingClientRect().left - wallItem[0].getBoundingClientRect().left;
   let cords = [];
+
   for (let i = 0; i < routes.length; i++) {
     for (let j = 0; j < routes[i].grips.length; j++) {
       if (routes[i].routeID !== selectedPath) {
         continue;
       }
+
       let top = routes[i].grips[j].points.x * topDiffrence - topDiffrence;
+
       if (top < 0) {
         top = 0;
       }
+
       let left = routes[i].grips[j].points.y * leftDiffrence - leftDiffrence;
+
       if (left < 0) {
         left = 0;
       }
+
       cords.push({
         xpoint: top,
         ypoint: left,
       });
     }
   }
+
   var points = cords;
   var Gen = d3
     .line()
@@ -85,15 +113,17 @@ const animatePath = function () {
 // #region ***  Callback-Visualisation - show___         ***********
 const listenToWindowResize = function () {
   window.addEventListener('resize', function () {
-    drawSelectedPath(+selectedPath);
+    drawSelectedPath();
   });
 };
 
 const showWall = function () {
   let html = '';
+
   for (let i = 0; i < 1420; i++) {
     html += '<div class="c-wall__grid--item"></div>';
   }
+
   html += '<svg id="gfg" width="200" height="200" class="c-wall__svg"></svg>';
   document.querySelector('.js-wall').innerHTML = html;
   console.info('Grid loaded');
@@ -101,6 +131,7 @@ const showWall = function () {
 
 const showRouteButtons = function () {
   let html = '';
+
   for (let i = 0; i < routes.length; i++) {
     let route_id = routes[i].routeID;
     let routeName = 'Route: ' + (i + 1);
@@ -108,29 +139,34 @@ const showRouteButtons = function () {
     let builder = routes[i].builder.lastname + ' ' + routes[i].builder.firstname;
     let rating = Math.round(routes[i].avgRating);
     let starSVG = '';
+
     for (let j = 0; j < 5; j++) {
       if (j < rating) {
-        starSVG += `<svg xmlns="http://www.w3.org/2000/svg" class="c-wall__star c-wall__star--fill" width="40" height="38" viewBox="0 0 40 38"><path d="M11.65,44,14.9,29.95,4,20.5l14.4-1.25L24,6l5.6,13.25L44,20.5,33.1,29.95,36.35,44,24,36.55Z" transform="translate(-4 -6)" /></svg>`;
+        starSVG += `<svg xmlns="http://www.w3.org/2000/svg"class="c-wall__star c-wall__star--fill"width="40"height="38"viewBox="0 0 40 38"><path d="M11.65,44,14.9,29.95,4,20.5l14.4-1.25L24,6l5.6,13.25L44,20.5,33.1,29.95,36.35,44,24,36.55Z"transform="translate(-4 -6)"/></svg>`;
       } else {
-        starSVG += `<svg xmlns="http://www.w3.org/2000/svg" class="c-wall__star" width="40" height="38" viewBox="0 0 40 38"><path d="M11.65,44,14.9,29.95,4,20.5l14.4-1.25L24,6l5.6,13.25L44,20.5,33.1,29.95,36.35,44,24,36.55Z" transform="translate(-4 -6)" /></svg>`;
+        starSVG += `<svg xmlns="http://www.w3.org/2000/svg"class="c-wall__star"width="40"height="38"viewBox="0 0 40 38"><path d="M11.65,44,14.9,29.95,4,20.5l14.4-1.25L24,6l5.6,13.25L44,20.5,33.1,29.95,36.35,44,24,36.55Z"transform="translate(-4 -6)"/></svg>`;
       }
     }
+
     let checked = '';
+
     if (i == 0) {
       checked = 'checked';
       selectedPath = route_id;
+      console.log('selected path: ', selectedPath);
     }
+
     html += `
-    <input class="o-hide-accessible c-options c-option--hidden js-path-select" type="radio" id="${i + 1}" name="routes" data-id="${route_id}" ${checked} /><label class="c-label c-label--options c-custom-options" for="${i + 1}">
-              <span class="u-mb-clear c-wall__rating--title">${routeName}</span><span class="u-mb-clear c-wall__rating--subtitle">Niveu: <span>${difficulty}</span></span>
-              <span class="u-mb-s c-wall__rating--subtitle">Builder: <span>${builder}</span></span>
-              <span class="u-mb-clear c-wall__rating--subtitle">Rating:</span>
-              <span>
-                ${starSVG}
-              </span>
-            </label>
-    `;
+    <input class="o-hide-accessible c-options c-option--hidden js-path-select"type="radio"id="${i + 1}"name="routes"data-id="${route_id}"${checked}/>
+    <label class="c-label c-label--options c-custom-options"for="${i + 1}">
+    <span class="u-mb-clear c-wall__rating--title">${routeName}</span>
+    <span class="u-mb-clear c-wall__rating--subtitle">Niveu: <span>${difficulty}</span>
+    </span><span class="u-mb-s c-wall__rating--subtitle">Builder: <span>${builder}</span>
+    </span><span class="u-mb-clear c-wall__rating--subtitle">Rating:</span>
+    <span>${starSVG}</span>
+    </label>`;
   }
+
   document.querySelector('.js-wall_route--options').innerHTML = html;
   console.info('Route buttons loaded');
   showGrips();
@@ -139,10 +175,13 @@ const showRouteButtons = function () {
 const showGrips = async function () {
   wall = document.querySelectorAll('.c-wall');
   wallItem = document.querySelectorAll('.c-wall__grid--item');
+
   for (let i = 0; i < routes.length; i++) {
     for (let j = 0; j < routes[i].grips.length; j++) {
       let index = routes[i].grips[j].points.x - 1 + (routes[i].grips[j].points.y - 1) * 20;
       wallItem[index].classList.add('c-wall__grid--item--grip');
+      wallItem[index].setAttribute('data-id', routes[i].routeID);
+
       switch (routes[i].grips[j].handgriptype) {
         case 'Bak':
           wallItem[index].classList.add('c-wall__grid--item--grip--bak');
@@ -171,7 +210,9 @@ const showGrips = async function () {
       }
     }
   }
-  drawSelectedPath(selectedPath);
+
+  drawSelectedPath();
+  hideNonSelectedRoutes();
   listenToRouteClick();
 };
 
@@ -202,27 +243,34 @@ const getRopes = async function () {
   const ropes = await fetchPromis(url);
   // showRopes(ropes);
 };
+
 // #endregion
 
 // #region ***  Event Listeners - listenTo___            ***********
 const listenToRouteClick = function () {
   let options = document.querySelectorAll('.js-path-select');
+
   options.forEach((option) => {
     option.addEventListener('change', function (event) {
-      selectedPath = this.dataset.id;
-      drawSelectedPath(selectedPath);
+      console.log('old ', selectedPath);
+      selectedPath = this.getAttribute('data-id');
+      drawSelectedPath();
+      hideNonSelectedRoutes();
     });
   });
 };
+
 // #endregion
 
 // #region ***  Init / DOMContentLoaded                  ***********
 document.addEventListener('DOMContentLoaded', function () {
   toggleNav();
   GetLogin();
+
   if (document.querySelector('#ropes')) {
     getRopes();
   }
+
   if (document.querySelector('#ropeDetail')) {
     getRopeId();
     showWall();
@@ -232,10 +280,11 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 // #endregion
 
-// region ***  User / login  ***********
+// #region ***  User / login  ***********
 const GetLogin = async function () {
   console.log('Checking login');
   let url = 'https://func-westeur-klimapp2.azurewebsites.net/auth/login';
+
   await fetch(url).then((res) => {
     if (res.status == 200) {
       console.log('logged in');
@@ -259,5 +308,9 @@ const showLogin = function (userData) {
 const logout = function () {
   console.log('logout');
   let url = 'https://func-westeur-klimapp2.azurewebsites.net/auth/logout';
-  fetch(url, { method: 'DELETE' });
+
+  fetch(url, {
+    method: 'DELETE',
+  });
 };
+// #endregion
